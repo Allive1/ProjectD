a1420cd714c3d27a9054224c1198538f9a3e3fb5
Omi Stark <Allive1@live.com>
2020-12-09 19:32:19 -0500

    Initial front end with material

--- d-front/src/app/app.module.ts
+++ d-front/src/app/app.module.ts
@@ -5,23 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import {MaterialModule} from "./material/material.module";
-import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ToolbarComponent,
-    ProfileComponent
+    ProfileComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule
-    // MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
